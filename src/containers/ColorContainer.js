0e72ef06595cb1e7430b8092be8b4161da7bd353
Jose <j.a.ojeda@live.com>
2019-10-30 14:08:33 -0700

    refactor App.js tino Color Container

---
+++ src/containers/ColorContainer.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import ColorDisplay from '../components/colors/ColorDisplay';
+import ColorPicker from '../components/colors/ColorPicker';
+
+export default class ColorContainer extends Component {
+  static propTypes = {
+
+  };
+
+  state = {
+    backgroundColor: 'black'
+  };
+
+  selectColor = backgroundColor => {
+    this.setState({ backgroundColor });
+  }
+
+  render() {
+    const { backgroundColor } = this.state;
+    const colors = ['red', 'green', 'black', 'yellow'];
+
+    return (
+      <>
+        <ColorDisplay backgroundColor={backgroundColor} />
+        <ColorPicker
+          selectColor={this.selectColor}
+          colors={colors} />
+      </>
+    );
+  }
+}
